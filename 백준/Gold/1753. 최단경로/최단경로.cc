cca5a130818b845f79f828f5acd1accccd379cdb
LeeTH916 <118820838+LeeTH916@users.noreply.github.com>
2023-05-19 01:24:59 +0900

    [Gold IV] Title: 최단경로, Time: 332 ms, Memory: 15516 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue>
+
+#define inf 210000000
+
+using namespace std;
+
+vector<pair<int, char>> map[20001];
+priority_queue <pair<int, int>> route;
+int dist[20001];
+char visited[20001];
+
+int main()
+{
+	int V, E, K;
+
+	cin >> V >> E;
+
+	cin >> K;
+
+	int u, v;
+	int w;
+
+	for (int i = 0; i < E; i++)
+	{
+		cin >> u >> v >> w;
+
+		map[u].push_back(make_pair(v,(char)w));
+	}
+
+	for (int i = 1; i <= V; i++)
+	{
+		dist[i] = inf;
+	}
+
+	dist[K] = 0;
+
+	route.push(make_pair(0, K));
+
+	while (!route.empty())
+	{
+		int cur = route.top().second;
+		int tmp_dist = -route.top().first;
+		route.pop();
+		if (visited[cur] == 1) continue;
+
+		visited[cur] = 1;
+		dist[cur] = tmp_dist;
+		
+
+		for (auto a : map[cur])
+		{
+			if (visited[a.first] == 0)
+			{
+				route.push(make_pair(-(a.second + dist[cur]), a.first));
+			}
+		}
+
+	}
+
+	for (int i = 1; i <= V; i++)
+	{
+		if (dist[i] == inf) cout << "INF" << '\n';
+		else cout << dist[i] << '\n';
+	}
+}
